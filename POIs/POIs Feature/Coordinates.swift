4343895e0c73720a4c86d23177eaac373a53a62c
Mate Csengeri <fic219@gmail.com>
2022-04-20 23:37:45 +0200

    Add POI model

---
+++ POIs/POIs Feature/Coordinates.swift
@@ -0,0 +1,12 @@
+//
+// Created by Mate Csengeri on 2022. 04. 20. at ___ORGANIZATIONNAME___
+// 
+//
+	
+
+import Foundation
+
+struct Coordinates {
+    let longitude: Double
+    let latitude: Double
+}
---
+++ POIs/POIs Feature/POI.swift
@@ -0,0 +1,15 @@
+//
+// Created by Mate Csengeri on 2022. 04. 20. at ___ORGANIZATIONNAME___
+// 
+//
+	
+
+import Foundation
+
+struct POI {
+    let name: String
+    let description: String?
+    let city: String
+    let imageURL: String
+    let coordinates: Coordinates
+}
