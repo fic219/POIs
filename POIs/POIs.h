4cb158be158cac91c1c48a5904b88e6c153985a2
Mate Csengeri <mate.csengeri@mondriaan.com>
2022-04-20 23:27:43 +0200

    Add POIs project

---
+++ POIs/POIs.h
@@ -0,0 +1,17 @@
+//
+// Created by Mate Csengeri on 2022. 04. 20. at ___ORGANIZATIONNAME___
+// 
+//
+	
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POIs.
+FOUNDATION_EXPORT double POIsVersionNumber;
+
+//! Project version string for POIs.
+FOUNDATION_EXPORT const unsigned char POIsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POIs/PublicHeader.h>
+
+
